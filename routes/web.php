c569f8bbd3bd4d50ac42315d3f2e756fe2672b4e
EC2 Default User <ec2-user@ip-172-31-35-17.ap-northeast-1.compute.internal>
2023-01-30 16:39:52 +0000

    Routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\Admin\NewsController;
+Route::controller(NewsController::class)->prefix('admin')->group(function() {
+    Route::get('news/create', 'add');
+});
+
+use App\Http\Controllers\Admin\profileController;
+Route::controller(profile::class)->prefix('admin')->group(function() {
+    Route::get('profile/create', 'add');
+    Route::get('profile/edit', 'edit');
+});
